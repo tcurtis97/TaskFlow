9ccc94c43ea2a048b3002e4b8fe381fe08fc97d3
Tim Curtis <timcurtis97@yahoo.com>
2021-06-08 14:50:28 -0500

    added fleet model

---
+++ nssCapstone-TaskFlow/TaskFlow/Models/Fleet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Models
+{
+    public class Fleet
+    {
+        public int Id { get; set; }
+
+        public int UserProfileId { get; set; }
+
+        public int Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+
+        public UserProfile userProfile { get; set; }
+    }
+}
